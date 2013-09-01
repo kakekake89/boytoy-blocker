3e76f4e457feb535bb6bb17b40767f8570d0db2b
kakekake89 <kakekake89@gmail.com>
2013-09-01 12:48:54 +0200

    rails new rack-tryout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RackTryout::Application.config.secret_token = 'd04e3e2135f9bf32197108c76a70b091034ba7ec8e381e6100b58596ec87c5d7f69e6ef64d1ca7f1092b6f54d6cf11911bc9bccd99f8ab9cd94ffda74e4e64d1'
